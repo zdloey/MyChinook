1e662c3e1bd145ab594a22cc048842ebdfdef961
Allan Anderson <allana@shaw.ca>
2017-05-14 13:38:06 -0700

    Day2

---
+++ MySolution/SolutionSystem/DAL/SolutionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+//using ChinookSystem.Data.Entities;
+#endregion
+
+
+namespace SolutionSystem.DAL
+{
+    internal class ChinookContext : DbContext
+    class SolutionContext
+    {
+    }
+}
