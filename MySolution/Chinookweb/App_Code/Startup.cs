f13dc1586d24ffd496615d8da49309964ab4542a
Allan Anderson <allana@shaw.ca>
2017-05-14 12:36:51 -0700

    create my solution

---
+++ MySolution/Chinookweb/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chinookweb.Startup))]
+namespace Chinookweb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
